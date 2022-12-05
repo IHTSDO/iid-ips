f86a97e4835a1507e326f41763045fc0d0487cad
alopezo <8TPFdVPnehjv4d>
2022-12-05 15:55:33 -0300

    Updated spec and build

--- src/app/coding-spec.ts
+++ src/app/coding-spec.ts
@@ -6,20 +6,18 @@ export const codingSpec = [
             {
                 title: 'Allergy intolerance substance (all SNOMED CT)',
                 type: 'autocomplete',
-                ecl: `< 762766007 | Edible substance (substance)| OR
-                < 425620007 | Metal (substance)| OR 
-                < 410942007 |Drug or medicament (substance)| OR 
-                < 373873005 |Pharmaceutical / biologic product (product)|`,
+                ecl: `< 105590001 |Substance (substance)| OR
+                < 373873005 |Pharmaceutical / biologic product (product)| OR 
+                < 418038007 |Propensity to adverse reactions to substance (finding)|`,
                 value: '',
-                note: 'Searching Allergy intolerance substance codes using a complete edition of SNOMED CT.'
+                note: `Searching problem list codes using a complete edition of SNOMED CT.`
             },
             {
                 title: 'Allergy intolerance substance (IPS Refset)',
                 type: 'autocomplete',
-                ecl: `(< 762766007 | Edible substance (substance)| OR
-                < 425620007 | Metal (substance)| OR 
-                < 410942007 |Drug or medicament (substance)| OR 
-                < 373873005 |Pharmaceutical / biologic product (product)|) AND 
+                ecl: `(< 105590001 |Substance (substance)| OR
+                < 373873005 |Pharmaceutical / biologic product (product)| OR 
+                < 418038007 |Propensity to adverse reactions to substance (finding)|) AND 
                 (^816080008 |International Patient Summary|)`,
                 value: '',
                 note: 'Searching Allergy intolerance substance codes using members of the IPS Refset only.'
@@ -35,7 +33,7 @@ export const codingSpec = [
                 type: 'autocomplete',
                 ecl: `< 404684003 |Clinical finding (finding)| OR 
                 < 272379006 |Event (event)| OR 
-                (< 243796009 |Situation with explicit context (situation)| : [0..0] 363589002 |Associated procedure (attribute)| = *)`,
+                < 243796009 |Situation with explicit context|`,
                 value: '',
                 note: 'Searching problem list codes using a complete edition of SNOMED CT.'
             },
@@ -44,7 +42,7 @@ export const codingSpec = [
                 type: 'autocomplete',
                 ecl: `(< 404684003 |Clinical finding (finding)| OR 
                 < 272379006 |Event (event)| OR 
-                (< 243796009 |Situation with explicit context (situation)| : [0..0] 363589002 |Associated procedure (attribute)| = *)) AND 
+                < 243796009 |Situation with explicit context|) AND 
                 (^816080008 |International Patient Summary|)`,
                 value: '',
                 note: 'Searching problem list codes using members of the IPS Refset only.'
@@ -52,71 +50,8 @@ export const codingSpec = [
         ]
     },
     {
-        title: 'Procedures',
-        description: 'In IPS, procedures are used to list and describe the history of clinical procedures performed in the patient, or the procedures for which results are reported.',
-        bindings: [
-            {
-                title: 'Procedures (all SNOMED CT)',
-                type: 'autocomplete',
-                ecl: `(< 71388002 |Procedure (procedure)|)`,
-                value: '',
-                note: 'Searching procedure codes using a complete edition of SNOMED CT.'
-            },
-            {
-                title: 'Procedures (IPS Refset)',
-                type: 'autocomplete',
-                ecl: `(< 71388002 |Procedure (procedure)|) AND 
-                (^816080008 |International Patient Summary|)`,
-                value: '',
-                note: 'Searching procedure codes using members of the IPS Refset only.'
-            }
-        ]
-    },
-    {
-        title: 'Medications',
-        description: 'Medications are used to list and describe the history of medications prescribed or consumed by the patient.',
-        bindings: [
-            {
-                title: 'Medications (all SNOMED CT)',
-                type: 'autocomplete',
-                ecl: `(< 373873005 |Pharmaceutical / biologic product (product)|)`,
-                value: '',
-                note: 'Searching medication codes using a complete edition of SNOMED CT.'
-            },
-            {
-                title: 'Medications (IPS Refset)',
-                type: 'autocomplete',
-                ecl: `(< 373873005 |Pharmaceutical / biologic product (product)|) AND 
-                (^816080008 |International Patient Summary|)`,
-                value: '',
-                note: 'Searching medication codes using members of the IPS Refset only.'
-            }
-        ]
-    },
-    {
-        title: 'Result Organisms',
-        description: 'This is a value set of the SNOMED CT organism concepts for use in the Results section observation values.',
-        bindings: [
-            {
-                title: 'Result Organisms (all SNOMED CT)',
-                type: 'autocomplete',
-                ecl: `(< 410607006 |Organism (organism)|)`,
-                value: '',
-                note: 'Searching result organism codes using a complete edition of SNOMED CT.'
-            },
-            {
-                title: 'Result Organisms (IPS Refset)',
-                type: 'autocomplete',
-                ecl: `(< 410607006 |Organism (organism)|) AND 
-                (^816080008 |International Patient Summary|)`,
-                value: '',
-                note: 'Searching result organism codes using members of the IPS Refset only.'
-            }
-        ]
-    },
-    {
-        title: 'Immunization',
-        description: 'This is a value set of the SNOMED CT immunization concepts.',
+        title: 'Immunizations',
+        description: 'The Immunizations Section defines a patient’s current immunization status and pertinent immunization history. The primary use case for the Immunization Section is to enable communication of a patient’s immunization status. The section includes current immunization status and the entire clinically pertinent immunization history that is known.',
         bindings: [
             {
                 title: 'Immunizations (all SNOMED CT)',
@@ -132,6 +67,69 @@ export const codingSpec = [
                 (^816080008 |International Patient Summary|)`,
                 value: '',
                 note: 'Searching immunization codes using members of the IPS Refset only.'
+            }
+        ]
+    },
+    {
+        title: 'Procedures',
+        description: 'In IPS, procedures are used to list and describe the history of clinical procedures performed in the patient, or the procedures for which results are reported.',
+        bindings: [
+            {
+                title: 'Procedures (all SNOMED CT)',
+                type: 'autocomplete',
+                ecl: `(< 71388002 |Procedure|) MINUS (<< 308335008 |Patient encounter procedure|)`,
+                value: '',
+                note: 'Searching procedure codes using a complete edition of SNOMED CT.'
+            },
+            {
+                title: 'Procedures (IPS Refset)',
+                type: 'autocomplete',
+                ecl: `((< 71388002 |Procedure|) MINUS (<< 308335008 |Patient encounter procedure|)) AND 
+                (^816080008 |International Patient Summary|)`,
+                value: '',
+                note: 'Searching procedure codes using members of the IPS Refset only.'
+            }
+        ]
+    },
+    {
+        title: 'Medications',
+        description: 'Medications are used to list and describe the history of medications prescribed or consumed by the patient.',
+        bindings: [
+            {
+                title: 'Medications (all SNOMED CT)',
+                type: 'autocomplete',
+                ecl: `(< 373873005 |Pharmaceutical / biologic product (product)|) MINUS (<<787859002 |Vaccine product (medicinal product)|)`,
+                value: '',
+                note: 'Searching medication codes using a complete edition of SNOMED CT.'
+            },
+            {
+                title: 'Medications (IPS Refset)',
+                type: 'autocomplete',
+                ecl: `((< 373873005 |Pharmaceutical / biologic product (product)|) MINUS (<<787859002 |Vaccine product (medicinal product)|)) AND 
+                (^816080008 |International Patient Summary|)`,
+                value: '',
+                note: 'Searching medication codes using members of the IPS Refset only.'
+            }
+        ]
+    },
+    {
+        title: 'Result Organisms',
+        description: 'This is a value set of the SNOMED CT organism concepts for use in the Results section observation values.',
+        bindings: [
+            {
+                title: 'Result Organisms (all SNOMED CT)',
+                type: 'autocomplete',
+                ecl: `(< 409822003 |Domain Bacteria (organism)| OR < 414561005 |Kingdom Fungi (organism)| OR < 49872002 |Virus (organism)|)`,
+                value: '',
+                note: 'Searching result organism codes using a complete edition of SNOMED CT.'
+            },
+            {
+                title: 'Result Organisms (IPS Refset)',
+                type: 'autocomplete',
+                ecl: `(< 409822003 |Domain Bacteria (organism)| OR < 414561005 |Kingdom Fungi (organism)| OR < 49872002 |Virus (organism)|) AND 
+                (^816080008 |International Patient Summary|)`,
+                value: '',
+                note: 'Searching result organism codes using members of the IPS Refset only.'
             }
         ]
     }
